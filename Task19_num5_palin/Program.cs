87b1a8d3167be8ab9f96dc3ae31bf3b6023a2dd4
Danya <theincredibledanzel@yandex.ru>
2022-06-16 14:13:15 +0400

    task19_bef_func

---
+++ Task19_num5_palin/Program.cs
@@ -0,0 +1,85 @@
+﻿int number = Input("Введите число: ");
+
+//bool result = true;
+
+if(number > 9999 && number < 100000)
+{
+    int pervrazr = number / 10000;
+    int pyatrazr = number % 10;
+    int vtorrazr = (number / 1000) % 10;
+    int chetrazr = (number % 100) / 10;
+    if(pervrazr == pyatrazr && vtorrazr == chetrazr)
+    {
+        Console.WriteLine("Да, Ваше число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Нет, Ваше число не является палиндромом");
+    }
+}
+else
+{
+    if(number > -100000 && number < -9999)
+    {
+        int pervrazr = number / -10000;
+        int pyatrazr = number % -10 * -1;
+        int vtorrazr = (number / -1000) % 10;
+        int chetrazr = (number % -100) / -10;
+        if(pervrazr == pyatrazr && vtorrazr == chetrazr)
+        {
+            Console.WriteLine("Да, Ваше число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Нет, Ваше число не является палиндромом");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели недопустимое число");
+    }
+}
+
+
+
+
+/*int PositiveNumber(int a)
+{
+    int pervrazr = a / 10000;
+    int pyatrazr = a % 10;
+    int vtorrazr = (a / 1000) % 10;
+    int chetrazr = (a % 100) / 10;
+    if(pervrazr == pyatrazr && vtorrazr == chetrazr)
+    {
+        result = true;
+    }
+    else
+    {
+        result = false;
+    }
+}
+
+
+int NegativeNumber(int a)
+{
+    int pervrazr = number / -10000;
+    int pyatrazr = number % -10 * -1;
+    int vtorrazr = (number / -1000) % 10;
+    int chetrazr = (number % -100) / -10;
+    if(pervrazr == pyatrazr && vtorrazr == chetrazr)
+    {
+        result = true;
+    }
+    else
+    {
+        result = false;
+    }
+}*/
+
+
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
