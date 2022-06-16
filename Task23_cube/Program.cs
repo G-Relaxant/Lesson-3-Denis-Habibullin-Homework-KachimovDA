7733433026f0e7f292f2c6ae0c446c60b6b49c3e
Danya <theincredibledanzel@yandex.ru>
2022-06-16 15:37:03 +0400

    task23

---
+++ Task23_cube/Program.cs
@@ -0,0 +1,31 @@
+﻿int number = Input("Введите число: ");
+
+int num = 1;
+
+if(number > 0)
+{
+    Console.Write("Кубы до вашего числа включительно: 1");
+    while(num < number)
+    {
+        Console.Write(", ");
+        num = num + 1;
+        int num2 = Cube(num);
+        Console.Write(num2);
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели недопустимое число");
+}
+
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Cube(int a)
+{
+    return a * a * a;
+}
